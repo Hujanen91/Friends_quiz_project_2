9715049dcc933852f10da132f2871e34fec10d78
Hujanen91 <>
2023-11-13 09:28:31 +0000

    Add try-again and lightning-round buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,15 @@ const rulesButton = document.getElementById('game-rules-button');
 const answerButton = document.getElementById('answer-buttons');
 const questionContainer = document.getElementById('question-container');
 const aboutGame = document.getElementById('about-game');
-const startButton = document.getElementById('start-button');
+const scoreValue = document.getElementById('scoreValue');
 const score = document.getElementById('score');
 let timer = document.getElementById('timer');
 
 // Set timer for each question
 const timePerQuestion = 20;
 
+// Variables that is gouing to loop
 
-// Hide question and 
+
+// Hide question and results container before clicking start
+
