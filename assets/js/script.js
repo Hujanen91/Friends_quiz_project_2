ceedd1fc893b8810085fce4187a6ab3659b5f326
Hujanen91 <>
2023-11-13 08:48:48 +0000

    Add variables in javascript

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+
+
+// Variables
+const startButton = document.getElementById('start-button');
+const rulesButton = document.getElementById('game-rules-button');
+const answerButton = document.getElementById('answer-buttons');
+const questionContainer = document.getElementById('question-container');
+const aboutGame = document.getElementById('about-game');
+const startButton = document.getElementById('start-button');
+const score = document.getElementById('score');
+let timer = document.getElementById('timer');
+
+// Set timer for each question
+const timePerQuestion = 20;
+
+
+// Hide question and 
