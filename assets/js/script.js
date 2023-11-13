95858b2631d07eae3f390e1f2000da652b22d502
Hujanen91 <>
2023-11-13 11:45:05 +0000

    Hide results, try again, lightning round and question buttons in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
 
 
+
+
 // Variables
 const startButton = document.getElementById('start-button');
 const rulesButton = document.getElementById('game-rules-button');
 const answerButton = document.getElementById('answer-buttons');
+const resultContainer = document.getElementById('results-container');
 const questionContainer = document.getElementById('question-container');
+const tryAgainContainer = document.getElementById('try-again-container')
 const aboutGame = document.getElementById('about-game');
 const scoreValue = document.getElementById('scoreValue');
 const tryAgain = document.getElementById('try-again');
@@ -12,11 +16,19 @@ const lightningRound = document.getElementById('lightning');
 const score = document.getElementById('score');
 let timer = document.getElementById('timer');
 
-// Set timer for each question
+// Set timer for lightning round
 const timePerQuestion = 20;
 
-// Variables that is gouing to loop
+// Variables that is going to loop
 
 
-// Hide question and results container before clicking start
+// Hide results, try-again, lightning-round and question buttons
+resultContainer.classList.add('hide');
+questionContainer.classList.add('hide');
+tryAgainContainer.classList.add('hide');
+
+// Make start and rules visible
+
+function runGame();
+
 
