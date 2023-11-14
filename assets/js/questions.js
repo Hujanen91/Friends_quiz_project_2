f6d5bc9060dd72be491ae6df906d1b9653778691
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2023-11-14 08:21:49 +0000

    Add 3 questions to questions.js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,29 +1,29 @@
 const questions = [
     {
-        question: "hello1",
+        question: "What is the profession of Chandler Bing for the main part of the show?",
         answer: [
-            {text: "one", correct: true},
-            {text: "two", correct: false},
-            {text: "three", correct: false},
-            {text: "four", correct: false}
+            {text: "IT procurement manager", correct: true},
+            {text: "Transponster", correct: false},
+            {text: "Paleontologist", correct: false},
+            {text: "Chef", correct: false}
         ]
     },
     {
-        question: "hello2",
+        question: "When Ross tries to teach his son Ben about Hanukkah, what does he dress up as?",
         answer: [
-            { text: "one", correct: true },
-            { text: "two", correct: false },
-            { text: "three", correct: false },
-            { text: "four", correct: false }
+            { text: "The Holiday Armadillo", correct: true },
+            { text: "A dreidel", correct: false },
+            { text: "A potato pancake", correct: false },
+            { text: "Santa Claus", correct: false }
         ]
     },
     {
-        question: "hello3",
+        question: "What is the name of Phoebe's alter ego?",
         answer: [
-            { text: "one", correct: true },
-            { text: "two", correct: false },
-            { text: "three", correct: false },
-            { text: "four", correct: false }
+            { text: "Regina Phalange", correct: true },
+            { text: "Smelly Nelly", correct: false },
+            { text: "Lisa Turtle", correct: false },
+            { text: "Gladys Glick", correct: false }
         ]
     }
 ]
