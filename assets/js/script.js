6afbb12512bde719ccf96ad70e94219920faef60
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2023-11-14 11:04:01 +0000

    Add back to start button and styled it

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,8 @@ const aboutGame = document.getElementById('about-game');
 const scoreValue = document.getElementById('scoreValue');
 const tryAgain = document.getElementById('try-again');
 const lightningRound = document.getElementById('lightning');
+const nextButton = document.getElementById('next-button');
+const backButton = document.getElementById('back-button')
 const score = document.getElementById('score');
 let timer = document.getElementById('timer');
 
@@ -20,6 +22,12 @@ var span = document.getElementsByClassName('close')[0];
 
 // Set timer for lightning round
 const timePerQuestion = 20;
+
+let shuffledQuestions;
+let currentQuestionIndex;
+let currentScore = 0; // Declare and initialize currentScore
+let timeLeft = timePerQuestion;
+let timerInterval;
 
 //Event listeners
 startButton.addEventListener('click', startGame);
@@ -32,13 +40,15 @@ function startGame() {
     rulesButton.classList.add('hide');
     aboutGame.classList.add('hide');
     timer.classList.add('hide');
+
+    shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+    currentQuestionIndex = 0;
+    currentScore = 0;
     questionContainer.classList.remove('hide');
     answerButton.classList.remove('hide');
     resultContainer.classList.remove('hide');
-    
-    shuffledQuestions = questions.sort(() => Math.random() - 0.5);
-    currentQuestionIndex = 0;
-
+    nextButton.classList.remove('hide');
+    backButton.classList.remove('hide');
 }
 
 //Next question
@@ -55,9 +65,7 @@ function selectAnswer() {
 //Time and score
 function timerScore() {
 
-
 }
-
 
 // Show modal when rules are clicked
 rules.onclick = function() {
