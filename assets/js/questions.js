a9f2f4aacc692448bbd3eb925ef61afcb938ad91
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2023-11-16 09:58:43 +0000

    Add more questions and answers to questions.js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,5 +1,5 @@
 const questions = [{
-    
+        //1
         question: "What is the profession of Chandler Bing for the main part of the show?",
         answers: [
             {text: "IT procurement manager", correct: true},
@@ -8,7 +8,7 @@ const questions = [{
             {text: "Chef", correct: false}
         ],
     },
-    {
+    {   //2
         question: "When Ross tries to teach his son Ben about Hanukkah, what does he dress up as?",
         answers: [
             { text: "The Holiday Armadillo", correct: true },
@@ -17,7 +17,7 @@ const questions = [{
             { text: "Santa Claus", correct: false }
         ],
     },
-    {
+    {   //3
         question: "What is the name of Phoebe's alter ego?",
         answers: [
             { text: "Regina Phalange", correct: true },
@@ -26,8 +26,8 @@ const questions = [{
             { text: "Gladys Glick", correct: false }
         ],
     },
-    {
-        question: "What does Ross famously scream when trying to help move a couch into his apartment?",
+    {   //4
+        question: "What does Ross scream when trying to help move a couch into his apartment?",
         answers: [
             { text: "Pivot!", correct: true },
             { text: "Lift and slide!", correct: false },
@@ -35,8 +35,152 @@ const questions = [{
             { text: "Shut Up! Shut Up! Shut Uuup!", correct: false }
         ],
     },
-    {
+    {   //5
         question: "What song does Ross want to play on the bagpipes at Monica and Chandlers wedding?",
+        answers: [
+            { text: "Scotland the Brave", correct: false },
+            { text: "Under pressure", correct: false },
+            { text: "Celebration", correct: true },
+            { text: "Disco inferno", correct: false }
+        ]
+    },
+    {   //6
+        question: "",
+        answers: [
+            { text: "Lift and slide!", correct: false },
+            { text: "My Sandwitch??", correct: false },
+            { text: "Shut Up! Shut Up! Shut Uuup!", correct: false },
+            { text: "Pivot!", correct: true }
+        ],
+    },
+    {   //7
+        question: "",
+        answers: [
+            { text: "Scotland the Brave", correct: false },
+            { text: "Under pressure", correct: false },
+            { text: "Celebration", correct: true },
+            { text: "Disco inferno", correct: false }
+        ]
+    },
+    {   //8
+        question: "Which country does Chandler accidentally book a trip to while trying to hide from Janice?",
+        answers: [
+            { text: "Yemen", correct: true },
+            { text: "Island", correct: false },
+            { text: "Sweden", correct: false },
+            { text: "Mexico", correct: false }
+        ],
+    },
+    {   //9
+        question: "Which character accidentally threw a woman's wooden leg into a fire?",
+        answers: [
+            { text: "Chandler", correct: false },
+            { text: "Joey", correct: true },
+            { text: "Rachel", correct: false },
+            { text: "Phoebe", correct: false }
+        ]
+    },
+    {   //10
+        question: "",
+        answers: [
+            { text: "Pivot!", correct: true },
+            { text: "Lift and slide!", correct: false },
+            { text: "My Sandwitch??", correct: false },
+            { text: "Shut Up! Shut Up! Shut Uuup!", correct: false }
+        ],
+    },
+    {   //11
+        question: "",
+        answers: [
+            { text: "Celebration", correct: true },
+            { text: "Scotland the Brave", correct: false },
+            { text: "Under pressure", correct: false },
+            { text: "Disco inferno", correct: false }
+        ]
+    },
+    {   //12
+        question: "",
+        answers: [
+            { text: "The Holiday Armadillo", correct: true },
+            { text: "A dreidel", correct: false },
+            { text: "A potato pancake", correct: false },
+            { text: "Santa Claus", correct: false }
+        ],
+    },
+    {   //13
+        question: "",
+        answers: [
+            { text: "Regina Phalange", correct: true },
+            { text: "Smelly Nelly", correct: false },
+            { text: "Lisa Turtle", correct: false },
+            { text: "Gladys Glick", correct: false }
+        ],
+    },
+    {   //14
+        question: "",
+        answers: [
+            { text: "Pivot!", correct: true },
+            { text: "Lift and slide!", correct: false },
+            { text: "My Sandwitch??", correct: false },
+            { text: "Shut Up! Shut Up! Shut Uuup!", correct: false }
+        ],
+    },
+    {   //15
+        question: "",
+        answers: [
+            { text: "Celebration", correct: true },
+            { text: "Scotland the Brave", correct: false },
+            { text: "Under pressure", correct: false },
+            { text: "Disco inferno", correct: false }
+        ]
+    },
+    {   //16
+        question: "",
+        answers: [
+            { text: "Pivot!", correct: true },
+            { text: "Lift and slide!", correct: false },
+            { text: "My Sandwitch??", correct: false },
+            { text: "Shut Up! Shut Up! Shut Uuup!", correct: false }
+        ],
+    },
+    {   //17
+        question: "",
+        answers: [
+            { text: "Celebration", correct: true },
+            { text: "Scotland the Brave", correct: false },
+            { text: "Under pressure", correct: false },
+            { text: "Disco inferno", correct: false }
+        ]
+    },
+    {   //18
+        question: "",
+        answers: [
+            { text: "Pivot!", correct: true },
+            { text: "Lift and slide!", correct: false },
+            { text: "My Sandwitch??", correct: false },
+            { text: "Shut Up! Shut Up! Shut Uuup!", correct: false }
+        ],
+    },
+    {   //19
+        question: "",
+        answers: [
+            { text: "Celebration", correct: true },
+            { text: "Scotland the Brave", correct: false },
+            { text: "Under pressure", correct: false },
+            { text: "Disco inferno", correct: false }
+        ]
+    },
+    {   //20
+        question: "",
+        answers: [
+            { text: "Pivot!", correct: true },
+            { text: "Lift and slide!", correct: false },
+            { text: "My Sandwitch??", correct: false },
+            { text: "Shut Up! Shut Up! Shut Uuup!", correct: false }
+        ],
+    },
+    {   //21
+        question: "",
         answers: [
             { text: "Celebration", correct: true },
             { text: "Scotland the Brave", correct: false },
